a5027b0df9a9c7ff4be245629b7b217ec85aae51
hoffmannc <hoffmann.colin@yahoo.de>
2023-01-19 00:46:57 +0100

    first commit

---
+++ model.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+
+
+class MyAwesomeModel(nn.Module):
+    def __init__(self):
+        super(MyAwesomeModel, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+        self.fc = nn.Linear(32 * 7 * 7, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+        output = self.fc(x)
+        return output
