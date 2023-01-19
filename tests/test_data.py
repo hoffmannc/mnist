df893330782c4430713331b5700ddce345593958
hoffmannc <hoffmann.colin@yahoo.de>
2023-01-19 15:43:37 +0100

    tests implemented

---
+++ tests/test_data.py
@@ -0,0 +1,14 @@
+import torch
+
+from tests import _PATH_DATA
+
+from data import mnist
+
+
+def test_data():
+    train_data, test_data = mnist()
+    train_images, train_labels = train_data
+    test_images, test_labels = test_data
+
+    assert train_images.size()[2:4] == torch.Size([28, 28]), "Unexpected image shape"
+    assert test_images.size()[2:4] == torch.Size([28, 28]), "Unexpected image shape"
